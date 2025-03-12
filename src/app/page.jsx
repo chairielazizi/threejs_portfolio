64cddd9c30d3c85e1eb386eb50224b2fbc3a00a4
chairielazizi <azizihr2000@gmail.com>
2025-03-12 16:03:10 +0800

    initial setup

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white underline">Home</h1>
+    </main>
+  );
+};
+
+export default Home;
