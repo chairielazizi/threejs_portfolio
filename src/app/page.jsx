ea16f48cb741bb49f461cd555cb46a9c2d154e51
chairielazizi <azizihr2000@gmail.com>
2025-03-12 16:51:06 +0800

    add constant folder, make the navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const Home = () => {
   return (
     <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white underline">Home</h1>
+      <Navbar />
     </main>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+"use client";
+import { navLinks } from "@/constants";
+import Link from "next/link";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+
+  return (
+    <header className="text-white fixed top-0 left-0 right-0 bg-black/90 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center mx-auto py-5 c-space">
+          <Link
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+          >
+            Chairiel
+          </Link>
+
+          <button
+            onClick={toggleMenu}
+            className="sm:hidden text-neutral-400 hover:text-white focus:outline-none flex"
+            aria-label="Toggle Menu"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="toggle"
+              className="h-6 w-6"
+            />
+          </button>
+
+          <nav className="hidden sm:flex">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+
+      {/* mobile */}
+      <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+const NavItems = () => {
+  return (
+    <ul className="nav-ul">
+      {/* {["Home", "About", "Projects", "Contact"].map((item, index) => (
+        <li key={index} className="nav-li">
+          <Link href="/" className="nav-li_a">
+            {item}
+          </Link>
+        </li>
+      ))} */}
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li">
+          <Link href={href} className="nav-li_a" onClick={() => {}}>
+            {name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Navbar;
