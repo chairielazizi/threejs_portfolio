45f1c699965c26708927dca74daee20fd99a91ce
chairielazizi <azizihr2000@gmail.com>
2025-03-20 01:12:04 +0800

    add avatar model for experience section, add some animations for the avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,14 @@
+"use client";
+import Avatar from "@/3d-models/Avatar";
+import CanvasLoader from "@/3d-models/CanvasLoader";
 import { workExperiences } from "@/constants";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
 
 const Experience = () => {
+  const [animationName, setAnimationName] = useState("salute");
+
   return (
     <section className="c-space my-20">
       <div className="w-full to-white-600">
@@ -9,13 +16,36 @@ const Experience = () => {
 
         <div className="work-container">
           {/* model grid */}
-          <div className="work-canvas">{/* <Canvas></Canvas> */}</div>
+          <div className="work-canvas">
+            <Canvas>
+              <ambientLight intensity={7} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <directionalLight position={[10, 10, 10]} intensity={1.5} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+              <Suspense fallback={<CanvasLoader />}>
+                <Avatar
+                  position-y={-3}
+                  scale={3}
+                  animationName={animationName}
+                />
+              </Suspense>
+            </Canvas>
+          </div>
+
           {/* experience grid */}
           <div className="work-content">
             <div className="py-5 px-2.5 sm:py-10 sm:px-5">
               {workExperiences.map(
                 ({ id, name, position, duration, title, icon, animation }) => (
-                  <div key={id} className="work-content_container group">
+                  <div
+                    key={id}
+                    className="work-content_container group"
+                    onClick={() => setAnimationName(animation.toLowerCase())}
+                    onPointerOver={() =>
+                      setAnimationName(animation.toLowerCase())
+                    }
+                    onPointerOut={() => setAnimationName("salute")}
+                  >
                     <div className="flex flex-col h-full items-center justify-start py-2">
                       <div className="work-content_logo">
                         <img src={icon} alt={name} className="w-full h-full" />
